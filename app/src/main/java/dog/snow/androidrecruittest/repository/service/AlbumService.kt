a9858d7ff4f062169c34adb2d93337db8bdad058
DESKTOP-H793V9V\Oliwia <monchagrin@outlook.com>
2020-09-08 23:34:04 +0200

    preparing services

--- app/src/main/java/dog/snow/androidrecruittest/repository/service/AlbumService.kt
+++ app/src/main/java/dog/snow/androidrecruittest/repository/service/AlbumService.kt
@@ -1,7 +1,14 @@
 package dog.snow.androidrecruittest.repository.service
 
 import dog.snow.androidrecruittest.repository.model.RawAlbum
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface AlbumService {
 
+    @GET("albums/{albumId}")
+    fun getAlbum(
+        @Path("albumId") albumId: Long
+    ): Single<RawAlbum>
 }
--- app/src/main/java/dog/snow/androidrecruittest/repository/service/UserService.kt
+++ app/src/main/java/dog/snow/androidrecruittest/repository/service/UserService.kt
@@ -1,7 +1,14 @@
 package dog.snow.androidrecruittest.repository.service
 
 import dog.snow.androidrecruittest.repository.model.RawUser
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface UserService {
 
+    @GET("users/{userId}")
+    fun getUser(
+        @Path("userId") userId: Long
+    ): Single<RawUser>
 }
