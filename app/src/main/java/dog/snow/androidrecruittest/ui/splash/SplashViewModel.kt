6f8522bfb3b8c1e468c77cee8dcb0e34627fc0f7
Oskar Rek <oskarrek98@gmail.com>
2020-09-09 22:46:31 +0200

    feat: create plash view model

---
+++ app/src/main/java/dog/snow/androidrecruittest/ui/splash/SplashViewModel.kt
@@ -0,0 +1,10 @@
+package dog.snow.androidrecruittest.ui.splash
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class SplashViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
