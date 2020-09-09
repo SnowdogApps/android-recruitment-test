d210778220619a6fe0b56b9a09df288efe7f9e6d
Oskar Rek <oskarrek98@gmail.com>
2020-09-09 22:49:02 +0200

    feat: add splash vm to module

--- app/src/main/java/dog/snow/androidrecruittest/di/module/ViewModelModule.kt
+++ app/src/main/java/dog/snow/androidrecruittest/di/module/ViewModelModule.kt
@@ -1,13 +1,22 @@
 package dog.snow.androidrecruittest.di.module
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import dog.snow.androidrecruittest.di.key.ViewModelKey
 import dog.snow.androidrecruittest.ui.common.view_model.ViewModelFactory
+import dog.snow.androidrecruittest.ui.splash.SplashViewModel
 
 @Module
 interface ViewModelModule {
 
     @Binds
     fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SplashViewModel::class)
+    fun bindSplashViewModel(splashViewModel: SplashViewModel): ViewModel
 }
