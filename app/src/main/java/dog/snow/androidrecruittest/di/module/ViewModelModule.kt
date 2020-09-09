48f6b0de78db45c09a2fa84545af658959217cdd
Oskar Rek <oskarrek98@gmail.com>
2020-09-09 14:30:15 +0200

    feat(di): setup base view model module

--- app/src/main/java/dog/snow/androidrecruittest/di/module/ViewModelModule.kt
+++ app/src/main/java/dog/snow/androidrecruittest/di/module/ViewModelModule.kt
@@ -1,8 +1,14 @@
 package dog.snow.androidrecruittest.di.module
 
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
 import dagger.Module
+import dog.snow.androidrecruittest.ui.common.view_model.ViewModelFactory
+import javax.inject.Singleton
 
 @Module
 interface ViewModelModule {
 
+    @Binds
+    fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 }
