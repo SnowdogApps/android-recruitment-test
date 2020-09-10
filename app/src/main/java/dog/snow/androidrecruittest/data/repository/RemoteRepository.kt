932345d91b1fdcddc2b03f5eddcfd2babe7d0855
Oskar Rek <oskarrek98@gmail.com>
2020-09-10 20:54:15 +0200

    feat: add method to remote repository implementation

--- app/src/main/java/dog/snow/androidrecruittest/data/repository/RemoteRepository.kt
+++ app/src/main/java/dog/snow/androidrecruittest/data/repository/RemoteRepository.kt
@@ -1,4 +1,9 @@
 package dog.snow.androidrecruittest.data.repository
 
+import dog.snow.androidrecruittest.data.model.photo.RawPhoto
+import dog.snow.androidrecruittest.data.source.remote.Resource
+import io.reactivex.Observable
+
 interface RemoteRepository {
+    fun fetchPhotos(): Observable<Resource<List<RawPhoto>>>
 }
--- app/src/main/java/dog/snow/androidrecruittest/data/repository/RemoteSource.kt
+++ app/src/main/java/dog/snow/androidrecruittest/data/repository/RemoteSource.kt
@@ -1,6 +1,12 @@
 package dog.snow.androidrecruittest.data.repository
 
+import dog.snow.androidrecruittest.data.model.photo.PhotoLimit
+import dog.snow.androidrecruittest.data.model.photo.RawPhoto
+import dog.snow.androidrecruittest.data.source.remote.Resource
 import dog.snow.androidrecruittest.data.source.remote.service.PhotoService
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -8,6 +14,13 @@ import javax.inject.Singleton
 class RemoteSource @Inject constructor(
     private val photoService: PhotoService
 ) : RemoteRepository {
+    override fun fetchPhotos(): Observable<Resource<List<RawPhoto>>> = photoService
+        .fetchPhotos(PhotoLimit(PHOTO_LIMIT))
+        .subscribeOn(Schedulers.io())
+        .switchMap {Observable.just(Resource.create(it))}
+        .observeOn(AndroidSchedulers.mainThread())
 
-
+    companion object {
+        private const val PHOTO_LIMIT = 100
+    }
 }
