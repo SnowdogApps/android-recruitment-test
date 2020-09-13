bb27d2c2ca5dbea2ef4cea8bcb77104e690c72cd
Oskar Rek <oskarrek98@gmail.com>
2020-09-13 12:51:21 +0200

    feat: add navigationui and room implementation

--- app/src/main/java/dog/snow/androidrecruittest/ui/page/splash/SplashActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/ui/page/splash/SplashActivity.kt
@@ -1,16 +1,23 @@
 package dog.snow.androidrecruittest.ui.page.splash
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.android.AndroidInjection
 import dog.snow.androidrecruittest.R
+import dog.snow.androidrecruittest.data.source.remote.Resource
 import dog.snow.androidrecruittest.ui.common.view_model.ViewModelFactory
 import dog.snow.androidrecruittest.utils.subscribe
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import java.io.File
 import javax.inject.Inject
 
 class SplashActivity : AppCompatActivity(R.layout.splash_activity) {
+
+    //TODO: add navigation and databinding libraries.
 
     @Inject //TODO: refactor this to be used by BaseActivity
     lateinit var viewModelFactory: ViewModelFactory
@@ -28,7 +35,7 @@ class SplashActivity : AppCompatActivity(R.layout.splash_activity) {
     private fun setupViewModel() {
         splashViewModel = ViewModelProvider(this, viewModelFactory)[SplashViewModel::class.java]
         splashViewModel.fetchDataState.subscribe(this) {
-            
+
         }
     }
 
